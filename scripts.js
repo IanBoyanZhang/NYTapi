6a1acdad8bb336b67c4c719a3c3873e7c7b8ee5c
Silly Knight <sillyknight1986@gmail.com>
2015-03-12 11:51:30 -0700

    First commit

---
+++ scripts.js
@@ -0,0 +1,44 @@
+var showData = function(i, result){
+  var template = $(".results .template .searchDisplay").clone();
+
+  var headline = template.find('.headline');
+}
+
+var KEY = require(['../APIkey/key']);
+var APIkey = KEY.APIkey;
+$(document).ready(function(){
+  $(':submit').on('click', function(){
+    var button = $(this).val();
+
+    var search = "?q=China";
+    var NYTuri = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+    // Request
+    var request = { tagged: search,
+                    site: 'New York Times',
+                    order: 'decs',
+                    sort: 'creation'};
+    // Ajax JSON
+    var result = $.ajax({ // ajax call starts
+        type:'GET',
+        url:NYTuri + search + '&fq=source:("The New York Times")&page=0&begin_date=20150101&sort=oldest&api-key='+APIkey,
+        data: search,
+        dataType:'json'
+    })
+
+    .done(function(result){
+      console.log("success");
+      console.log(result.response);
+      console.log(result.response.docs[2].web_url);
+
+    }).fail(function(){
+      console.log("error");
+      $('.results').html('This feature is not working.');
+    });
+    $('#info').html('');
+    if (button === 'All'){
+      $('#info').append('Red Wine: ' + '<br/>');
+    }
+    return false; // keeps the page from not refreshing
+  });
+});
