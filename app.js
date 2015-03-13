456f9a98f2744c00712779cb4f86f0a69e9c4143
Silly Knight <sillyknight1986@gmail.com>
2015-03-12 17:24:55 -0700

    index.html

--- app.js
+++ app.js
@@ -25,20 +25,18 @@ $(document).ready(function(){
         data: search,
         dataType:'json'
     })
-
     .done(function(result){
       console.log("success");
-      console.log(result.response);
+      $('#info').html('');
+      $('#info').append(result.response.docs[2].headline.main + '<br/>');
       console.log(result.response.docs[2].web_url);
 
-    }).fail(function(){
+
+     }).fail(function(){
       console.log("error");
       $('.results').html('This feature is not working.');
     });
-    $('#info').html('');
-    if (button === 'All'){
-      $('#info').append('Red Wine: ' + '<br/>');
-    }
+
     return false; // keeps the page from not refreshing
   });
 });
