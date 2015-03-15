da98468a8ae9a350629436db38c110c7cf7317a7
Silly Knight <sillyknight1986@gmail.com>
2015-03-14 19:50:36 -0700

    NYT checkout

--- static/js/app.js
+++ static/js/app.js
@@ -1,41 +1,72 @@
-var showData = function(i, result){
-  var template = $(".results .template .searchDisplay").clone();
-
-  var headline = template.find('.headline');
-}
-
 var KEY = require(['../APIkey/key']);
 var APIkey = KEY.APIkey;
+
+function infoAppend(data){
+      if (typeof data !== "string"){
+        console.error("info append function input args is not string!");
+      }
+      $('#info').html('');
+      $('#info').append("Input date is void, setting search parameter to " + data + '<br/>');
+}
+
 $(document).ready(function(){
   $(':submit').on('click', function(){
     var button = $(this).val();
+    var searchText = document.getElementById("searchText").value;
+    var beginDate= document.getElementById("beginDate").value;
+    var endDate=document.getElementById('endDate').value;
+    
+    if(searchText === ''){
+      searchText = "New York Times";
+      infoAppend(searchText);
+    }
+    
+    if(beginDate=== ''){
+      beginDate = "2015-01-01";
+      infoAppend(beginDate);
+    }
 
-    var search = "?q=China";
+    if(endDate=== ''){
+      endDate = "2015-01-01";
+      infoAppend(endDate);
+    }
+
+    // Process search date
+    beginDate= beginDate.split("-").join("");
+    console.log(beginDate);
     var NYTuri = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
+    var searchBeginDate = "&begin_date"+beginDate;
+    var searchEndDate = "&end_date"+endDate;
 
     // Request
-    var request = { tagged: search,
+      var request = { tagged: searchText,
                     site: 'New York Times',
                     order: 'decs',
                     sort: 'creation'};
     // Ajax JSON
-    var result = $.ajax({ // ajax call starts
+      var result = $.ajax({ // ajax call starts
         type:'GET',
-        url:NYTuri + search + '&fq=source:("The New York Times")&page=0&begin_date=20150101&sort=oldest&api-key='+APIkey,
-        data: search,
+        url:NYTuri + "?q=" + searchText + "&page=0"
+            + searchBeginDate + searchBeginDate + "&api-key=" + APIkey,
+ //       url:"http://api.nytimes.com/svc/search/v2/articlesearch.json?q=New York Times&page=0&begin_date20150101&api-key=1f2c2c3a299f6ba19c89f8ec7ebb5b96:2:71564068",
+        data: searchText,
         dataType:'json'
-    })
-    .done(function(result){
-      console.log("success");
-      $('#info').html('');
-      $('#info').append(result.response.docs[2].headline.main + '<br/>');
-      console.log(result.response.docs[2].web_url);
-
-     }).fail(function(){
-      console.log("error");
-      $('.results').html('This feature is not working.');
-    });
-
+      })
+      .done(function(result){
+        var docLength = result.response.docs.length;
+        console.log(docLength);
+        $('#info').html('');
+        $('#info').append('<em>' + "The feature will list first" + " " + docLength.toString() + " " +
+                          "title(s) of search result" + '</em>' +'<br/>' + '<br/>');
+        for (var iter = 0; iter < docLength; ++iter){
+           $('#info').append('<a href=" '+result.response.docs[iter].web_url+' ">' + 
+                          result.response.docs[iter].headline.main + '</a>' + '<br/>' + '<br/>');
+        }
+       }).fail(function(){
+        $('#info').html('');
+        console.log("error");
+        $('.results').html('This feature is not working.');
+      });
     return false; // keeps the page from not refreshing
   });
 });
